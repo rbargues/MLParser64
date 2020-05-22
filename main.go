62464b5091ddc4cd8de029d2db99ef8e7c17976d
rbargues <rbargues@gmail.com>
2020-05-22 16:09:26 -0400

    working on screenshot logic

--- main.go
+++ main.go
@@ -20,34 +20,50 @@ func main() {
     jsonFile := readJSON()
     whiteScreenCt := 0
     blackScreenCt := 0
+    var grabLevelName bool
+    var startTime time.Time
     for true {
         screenshotGrab(r1, r2)
-        whiteScreenBool := whiteScreen()
-        if whiteScreenBool == true {
-            whiteScreenCt = 1
-        }
-        blackScreenBool := blackScreen()
-        if blackScreenBool == true && whiteScreenCt == 1 {
-            // should be no instance where we have black then white again
-            whiteScreenCt = 0 
-            time.Sleep(5 * time.Second)
-            screenshotGrab(r1, r2)
-            imgCrop("screenshot.png")
-            dctArr := obtainDCT("cropped.png")
-            phashVal := phash(dctArr)
-            minDist := math.Inf(0)
-            currentLevel := ""
-            for key, value := range jsonFile {
-                hamming := hammingDistance(phashVal, value)
-                if float64(hamming) < minDist {
-                    minDist = float64(hamming)
-                    currentLevel = key
-                }
-            }
- 
-            fmt.Printf("%v\n", currentLevel)
-        } else if blackScreenBool == true {
+        if whiteScreen() {
+            whiteScreenCt ++
+        } else if blackScreen() {
             blackScreenCt ++
         }
+        if whiteScreenCt == 1 || (blackScreenCt == 1 && whiteScreenCt == 0) {
+            startTime = time.Now()
+        } else if (whiteScreenCt == 1 && blackScreenCt == 1) || (blackScreenCt == 3 && whiteScreenCt == 0) {
+            // will need to handle levels where there are only 2 black screens
+            // princess secret slide etc
+            // maybe have dct on static portion of level
+            whiteScreenCt = 0
+            blackScreenCt = 0
+            endTime := time.Since(startTime)
+            fmt.Printf("%v\n", endTime)
+            time.Sleep(5 * time.Second)
+        }
+
+        // blackScreenBool := blackScreen()
+        // if blackScreenBool == true && whiteScreenCt == 1 {
+        //     // should be no instance where we have black then white again
+        //     whiteScreenCt = 0 
+        //     time.Sleep(5 * time.Second)
+        //     screenshotGrab(r1, r2)
+        //     imgCrop("screenshot.png")
+        //     dctArr := obtainDCT("cropped.png")
+        //     phashVal := phash(dctArr)
+        //     minDist := math.Inf(0)
+        //     currentLevel := ""
+        //     for key, value := range jsonFile {
+        //         hamming := hammingDistance(phashVal, value)
+        //         if float64(hamming) < minDist {
+        //             minDist = float64(hamming)
+        //             currentLevel = key
+        //         }
+        //     }
+ 
+        //     fmt.Printf("%v\n", currentLevel)
+        // } else if blackScreenBool == true {
+        //     blackScreenCt ++
+        // }
     }
 }
