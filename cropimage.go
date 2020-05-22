71cd8ba878bff063c848bf8d52ad4412c1383fa0
rbargues <rbargues@gmail.com>
2020-05-22 16:53:56 -0400

    added different cropping logic for black screen levels

--- cropimage.go
+++ cropimage.go
@@ -8,7 +8,7 @@ import (
 	"math"
  )
 
-func imgCrop(fname string){
+func whiteImgCrop(fname string){
 	cropCopy, _ := os.Open(fname)
 	//630, 470
 	defer cropCopy.Close()
@@ -31,4 +31,30 @@ func imgCrop(fname string){
 	defer newImg.Close()
 
 png.Encode(newImg, m)
+}
+
+func blackImgCrop(fname string){
+	cropCopy, _ := os.Open(fname)
+	//630, 470
+	// 270-270 860-330
+	defer cropCopy.Close()
+	cropInfo, _ := png.Decode(cropCopy)
+
+	bounds := cropInfo.Bounds()
+	imgWidth := int((math.Round(float64(bounds.Max.X) * (float64(590.0) / float64(1015.0)))))
+	imgHeight := int((math.Round(float64(bounds.Max.Y) * (float64(60) / float64(720)))))
+
+
+	// used to start cropping
+	newX := int(math.Round(float64(bounds.Max.X) * float64(270.0 / 1015.0)))
+	newY := int(math.Round(float64(bounds.Max.Y) * float64(270.0 / 720.0)))
+
+	m := image.NewRGBA(image.Rect(0, 0, imgWidth, imgHeight))
+	draw.Draw(m, image.Rect(0, 0, imgWidth, imgHeight), cropInfo, image.Point{newX, newY}, draw.Src)
+
+	newFilePath := "cropped.png"
+	newImg, _ := os.Create(newFilePath)
+	defer newImg.Close()
+
+	png.Encode(newImg, m)
 }
--- main.go
+++ main.go
@@ -1,69 +1,69 @@
 package main
 
 import (
-	"fmt"
+	// "fmt"
     "time"
-    "math"
+    // "math"
 )
 
 //takes 25 seconds from last bowser star until final black screen
 func main() {
-    //starts the timer
-    timer := make(chan time.Time)
-    go startgame(timer)
-    timeVal := <- timer
-
     var r1 [2]int
     r1[0], r1[1] = 65, 80
     var r2 [2]int
     r2[0], r2[1] = 1080, 800
-    jsonFile := readJSON()
-    whiteScreenCt := 0
-    blackScreenCt := 0
-    var grabLevelName bool
-    var startTime time.Time
+
     for true {
         screenshotGrab(r1, r2)
-        if whiteScreen() {
-            whiteScreenCt ++
-        } else if blackScreen() {
-            blackScreenCt ++
-        }
-        if whiteScreenCt == 1 || (blackScreenCt == 1 && whiteScreenCt == 0) {
-            startTime = time.Now()
-        } else if (whiteScreenCt == 1 && blackScreenCt == 1) || (blackScreenCt == 3 && whiteScreenCt == 0) {
-            // will need to handle levels where there are only 2 black screens
-            // princess secret slide etc
-            // maybe have dct on static portion of level
-            whiteScreenCt = 0
-            blackScreenCt = 0
-            endTime := time.Since(startTime)
-            fmt.Printf("%v\n", endTime)
+        if blackScreen() {
             time.Sleep(5 * time.Second)
+            screenshotGrab(r1, r2)
+            break
         }
-
-        // blackScreenBool := blackScreen()
-        // if blackScreenBool == true && whiteScreenCt == 1 {
-        //     // should be no instance where we have black then white again
-        //     whiteScreenCt = 0 
-        //     time.Sleep(5 * time.Second)
-        //     screenshotGrab(r1, r2)
-        //     imgCrop("screenshot.png")
-        //     dctArr := obtainDCT("cropped.png")
-        //     phashVal := phash(dctArr)
-        //     minDist := math.Inf(0)
-        //     currentLevel := ""
-        //     for key, value := range jsonFile {
-        //         hamming := hammingDistance(phashVal, value)
-        //         if float64(hamming) < minDist {
-        //             minDist = float64(hamming)
-        //             currentLevel = key
-        //         }
-        //     }
- 
-        //     fmt.Printf("%v\n", currentLevel)
-        // } else if blackScreenBool == true {
-        //     blackScreenCt ++
-        // }
     }
+    //     if grabLevelName {
+            
+    //     }
+    //     if whiteScreen() {
+    //         whiteScreenCt ++
+    //     } else if blackScreen() {
+    //         blackScreenCt ++
+    //     }
+    //     if whiteScreenCt == 1 || (blackScreenCt == 1 && whiteScreenCt == 0) {
+    //         startTime = time.Now()
+    //     } else if (whiteScreenCt == 1 && blackScreenCt == 1) || (blackScreenCt == 3 && whiteScreenCt == 0) {
+    //         // will need to handle levels where there are only 2 black screens
+    //         // princess secret slide etc
+    //         // maybe have dct on static portion of level
+    //         whiteScreenCt = 0
+    //         blackScreenCt = 0
+    //         endTime := time.Since(startTime)
+    //         fmt.Printf("%v\n", endTime)
+    //         time.Sleep(5 * time.Second)
+    //     }
+
+    //     // blackScreenBool := blackScreen()
+    //     // if blackScreenBool == true && whiteScreenCt == 1 {
+    //     //     // should be no instance where we have black then white again
+    //     //     whiteScreenCt = 0 
+    //     //     time.Sleep(5 * time.Second)
+    //     //     screenshotGrab(r1, r2)
+    //     //     imgCrop("screenshot.png")
+    //     //     dctArr := obtainDCT("cropped.png")
+    //     //     phashVal := phash(dctArr)
+    //     //     minDist := math.Inf(0)
+    //     //     currentLevel := ""
+    //     //     for key, value := range jsonFile {
+    //     //         hamming := hammingDistance(phashVal, value)
+    //     //         if float64(hamming) < minDist {
+    //     //             minDist = float64(hamming)
+    //     //             currentLevel = key
+    //     //         }
+    //     //     }
+ 
+    //     //     fmt.Printf("%v\n", currentLevel)
+    //     // } else if blackScreenBool == true {
+    //     //     blackScreenCt ++
+    //     // }
+    // }
 }
